026123c318af8d56efe57b6362a53af277fcaeb7
andrew95s <soriano2020s@gmail.com>
2024-04-21 15:08:43 -0500

    Entrega Web, modificacion backend4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
-from dj_database_url
+import dj_database_url
 import os
 
 load_dotenv()
